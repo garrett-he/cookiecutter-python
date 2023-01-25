ae3b235077c0379968dde4a60cb654aa1f3c06ea
Garrett HE <garrett.he@outlook.com>
2023-01-25 11:12:02 +0800

    Project: Add support for cython

---
+++ tests/test_bake_cython.py
@@ -0,0 +1,13 @@
+from pytest_cookies.plugin import Cookies
+
+
+def test_bake_cython(cookies: Cookies):
+    result = cookies.bake(extra_context={'with_pyinstaller': 'yes', 'with_cli': 'yes', 'with_cython': 'yes'})
+    assert not result.exception
+
+    assert 'cython' in result.project_path.joinpath('pyproject.toml').read_text()
+
+    result = cookies.bake(extra_context={'with_cython': 'no'})
+    assert not result.exception
+
+    assert 'cython' not in result.project_path.joinpath('pyproject.toml').read_text()
