74f479d1d38a1fe68041a4994b0e71c60c510346
Garrett HE <garrett.he@outlook.com>
2023-01-26 10:38:57 +0800

    Project: Add Makefile file

--- tests/test_bake_cython.py
+++ tests/test_bake_cython.py
@@ -6,8 +6,10 @@ def test_bake_cython(cookies: Cookies):
     assert not result.exception
 
     assert 'cython' in result.project_path.joinpath('pyproject.toml').read_text()
+    assert 'poetry run python setup.py build_ext --build-lib build' in result.project_path.joinpath('Makefile').read_text()
 
     result = cookies.bake(extra_context={'with_cython': 'no'})
     assert not result.exception
 
     assert 'cython' not in result.project_path.joinpath('pyproject.toml').read_text()
+    assert 'poetry run python setup.py build_ext --build-lib build' not in result.project_path.joinpath('Makefile').read_text()
--- tests/test_bake_pyinstaller.py
+++ tests/test_bake_pyinstaller.py
@@ -7,9 +7,11 @@ def test_bake_pyinstaller(cookies: Cookies):
 
     assert 'pyinstaller' in result.project_path.joinpath('pyproject.toml').read_text()
     assert result.project_path.joinpath(f'{result.context["project_slug"]}.spec').exists()
+    assert 'poetry run pyinstaller' in result.project_path.joinpath('Makefile').read_text()
 
     result = cookies.bake(extra_context={'with_pyinstaller': 'no'})
     assert not result.exception
 
     assert 'pyinstaller' not in result.project_path.joinpath('pyproject.toml').read_text()
     assert not result.project_path.joinpath(f'{result.context["project_slug"]}.spec').exists()
+    assert 'poetry run pyinstaller' not in result.project_path.joinpath('Makefile').read_text()
