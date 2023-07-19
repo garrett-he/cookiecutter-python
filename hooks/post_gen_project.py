a71d37a164738a2efde86af8c3acc0ac1c5cf4bc
Garrett HE <garrett.he@outlook.com>
2023-07-19 12:50:18 +0800

    Template: Add configuration file for pyinstaller

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,6 +3,7 @@ from glob import glob
 
 license_id = '{{ cookiecutter.license_id }}'
 with_click = '{{ cookiecutter.with_click }}'
+with_pyinstaller = '{{ cookiecutter.with_pyinstaller }}'
 
 if license_id != 'Unlicense':
     os.rename('LICENSE.{{ cookiecutter.license_id }}', 'LICENSE')
@@ -14,3 +15,6 @@ for license_file in glob('LICENSE.*'):
 if with_click == 'no':
     os.unlink('src/{{ cookiecutter.project_package }}/__main__.py')
     os.unlink('tests/test_cli.py')
+
+if with_pyinstaller == 'no':
+    os.unlink('{{ cookiecutter.project_slug }}.spec')
