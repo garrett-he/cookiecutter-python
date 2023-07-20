76e0e137744e0da32c1da673446e33f1e0806c9b
Garrett HE <garrett.he@outlook.com>
2023-07-20 10:40:34 +0800

    Template: Add specification file for make

--- tests/test_bake_template.py
+++ tests/test_bake_template.py
@@ -134,9 +134,11 @@ def test_bake_pyinstaller(cookies: Cookies):
 
     assert 'pyinstaller' in result.project_path.joinpath('pyproject.toml').read_text()
     assert result.project_path.joinpath(f'{result.context["project_slug"]}.spec').exists()
+    assert f'poetry run pyinstaller {result.context["project_slug"]}.spec' in result.project_path.joinpath('Makefile').read_text()
 
     result = cookies.bake(extra_context={'with_pyinstaller': 'no'})
     assert not result.exception
 
     assert 'pyinstaller' not in result.project_path.joinpath('pyproject.toml').read_text()
     assert not result.project_path.joinpath(f'{result.context["project_slug"]}.spec').exists()
+    assert f'poetry run pyinstaller {result.context["project_slug"]}.spec' not in result.project_path.joinpath('Makefile').read_text()
