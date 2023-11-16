216b45131436688d5a487a82103a3bab6f2539bb
Garrett HE <garrett.he@outlook.com>
2023-11-16 11:17:57 +0800

    Project: Add support for pyinstaller

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,6 +5,7 @@ from glob import glob
 license_id = '{{ cookiecutter.license_id }}'
 with_cli = '{{ cookiecutter.with_cli }}'
 with_gui = '{{ cookiecutter.with_gui }}'
+with_pyinstaller = '{{ cookiecutter.with_pyinstaller }}'
 
 os.rename(f'LICENSE.{license_id}', 'LICENSE')
 
@@ -25,3 +26,12 @@ if with_cli == 'no':
 if with_gui == 'no':
     os.unlink('src/{{ cookiecutter.project_package }}/__gui__.py')
     shutil.rmtree('src/{{ cookiecutter.project_package }}/gui')
+
+if with_pyinstaller == 'yes':
+    if with_cli == 'no':
+        os.unlink('{{ cookiecutter.project_slug }}-cli.spec')
+    if with_gui == 'no':
+        os.unlink('{{ cookiecutter.project_slug }}-gui.spec')
+else:
+    os.unlink('{{ cookiecutter.project_slug }}-cli.spec')
+    os.unlink('{{ cookiecutter.project_slug }}-gui.spec')
